96f86ce18339d96ced48570ba266cce1cc85616a
Marcin Dykowski <marcin4a2016@wp.pl>
2025-10-16 13:39:54 +0200

    adding feature 2

--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -3,7 +3,16 @@ from src.account import Account
 
 class TestAccount:
     def test_account_creation(self):
-        account = Account("John", "Doe")
+        account = Account("John", "Doe", "12345678901")
         assert account.first_name == "John"
         assert account.last_name == "Doe"
         assert account.balance == 0
+        assert account.pesel == "12345678901"
+    
+    def test_wrong_pesel_short(self):
+        account = Account("Jane", "Doe", "12345")
+        assert account.pesel == "Invalid"
+    
+    def test_wrong_pesel_long(self):
+        account = Account("Jane", "Doe", "123454323244234224242")
+        assert account.pesel == "Invalid"
