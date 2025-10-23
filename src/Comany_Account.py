b2c67febedc6e2f0295799a7929dc6038906ba06
Marcin Dykowski <marcin4a2016@wp.pl>
2025-10-23 18:49:03 +0200

    adding feature 7

--- src/Comany_Account.py
+++ src/Comany_Account.py
@@ -4,6 +4,7 @@ class CompanyAccount(Account):
     def __init__ (self, company_name, nip):
         self.company_name = company_name
         self.nip = nip
+        self.balance = 0
 
-        if (len(company_name) != 10):
-            self.company_name = "Invalid"
+        if (len(nip) != 10):
+            self.nip = "Invalid"
---
+++ tests/unit/test_money_transfer.py
@@ -0,0 +1,50 @@
+from src.personal_acount import Personal_Account
+from src.Comany_Account import CompanyAccount
+
+class Test_transfer:
+    def test_getting_money(self):
+        account = Personal_Account("John", "Doe", "05210700056")
+        account.getting_money(100)
+        assert account.balance == 100
+    
+    def test_getting_money_with_promo_code(self):
+        account = Personal_Account("John", "Doe", "05210700056", "PROM_XYZ")
+        account.getting_money(100)
+        assert account.balance == 150
+
+    def test_outgoing_trasnfer(self):
+        account = Personal_Account("John", "Doe", "05210700056")
+        account.balance = 100
+        account.outgoing_transer(50)
+        assert account.balance == 50
+
+    def test_outgoing_trasnfer_exeding_ammout(self):
+        account = Personal_Account("John", "Doe", "05210700056")
+        account.balance = 100
+        account.outgoing_transer(500)
+        assert account.balance == 100
+
+    def test_outgoing_trasnfe_negative(self):
+        account = Personal_Account("John", "Doe", "05210700056")
+        account.balance = 100
+        account.outgoing_transer(-50)
+        assert account.balance == 100
+
+    # company transfers
+    def test_Company_money_transfer(self):
+        account = CompanyAccount("BANK_22", "1234567890")
+        account.balance = 10000
+        account.getting_money(5000)
+        assert account.balance == 15000
+
+    def test_Company_outgoing_money(self):
+        account = CompanyAccount("Bank_33","1234567890")
+        account.balance = 10000
+        account.outgoing_transer(5000)
+        assert account.balance == 5000  
+
+    def test_Company_outgoing_money_to_much(self):
+        account = CompanyAccount("Bank_33","1234567890")
+        account.balance = 10000
+        account.outgoing_transer(50000)
+        assert account.balance == 10000  
